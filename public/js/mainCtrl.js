a36f2e3ed1498c61228255ca6f7c5ac8d22adb68
sbrycebarker <sbrycesti@gmail.com>
2017-04-19 17:18:14 -0600

    data!

--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -8,14 +8,12 @@ angular.module('myApp').controller('mainCtrl', function($scope, mainService) {
 
   // $scope.getinsta();
 
-  $scope.getinstaimg = function() {
-    mainService.getinsta().then(function(result) {
-      console.log(result)
-      $scope.imgs = result.data.data
-    })
-  }
+  mainService.getinsta().then(function(result) {
+    console.log(result)
+    $scope.feed = result.data.data;
+  })
 
-  $scope.getinstaimg();
+
 
   // var feed = new Instafeed({
   //     get: 'user',
